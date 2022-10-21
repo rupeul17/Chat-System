f526e1d54f634d019a05704fa69fc2063d332269
rupeul17 <56832593+rupeul17@users.noreply.github.com>
2022-10-21 14:52:16 +0900

    chat 시스템 개발

---
+++ login/script.js
@@ -0,0 +1,36 @@
+const id = document.getElementById('id')
+const password = document.getElementById('password')
+const login = document.getElementById('login')
+let errStack = 0;
+
+login.addEventListener('click', () => {
+
+    if (id.value == 'acid') {
+        if (password.value == '0000') {
+            alert('로그인 되었습니다!')
+            var obj = new Object();
+            obj.msgtype = "LOGIN"
+            obj.userid = id.value;
+            obj.userpw = password.value;
+            obj.msg = "로그인 되었습니다!"
+
+            var ws = new WebSocket('ws://' + window.location.host + '/ws');
+            ws.onopen = function(event) {
+                ws.send(JSON.stringify(obj));
+            }
+            location.replace('main.html')
+            
+        }
+        else {
+            alert('아이디와 비밀번호를 다시 한 번 확인해주세요!')
+            errStack ++;
+        }
+    }
+    else {
+        alert('존재하지 않는 계정입니다.')
+    }
+
+    if (errStack >= 5) {
+        alert('비밀번호를 5회 이상 틀리셨습니다. 비밀번호 찾기를 권장드립니다.')
+    }
+})
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,41 +9,106 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type ClientMsgInfo struct {
+	MsgType string
+	UserId  string
+	UserPw  string
+	Msg     []byte
+}
+
+type Message struct {
+	MsgType int
+	MsgInfo ClientMsgInfo
+}
+
+var Clients = make(map[*websocket.Conn]bool)
+var Broadcast = make(chan Message)
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
+func BroadcastHandler() {
+
+	for {
+		select {
+		case Msg, ok := <-Broadcast:
+			if ok {
+				for conn := range Clients {
+					if err := conn.WriteMessage(Msg.MsgType, Msg.MsgInfo.Msg); err != nil {
+						log.Printf("conn.WriteMessage : %v\n", err)
+						return
+					}
+				}
+			}
+		}
+	}
+}
+
 func socketHandler(w http.ResponseWriter, r *http.Request) {
+	/*
+		요청 및 응답을 http -> ws 로 변경한다.
+	*/
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Printf("upgrader.Upgrade: %v\n", err)
 		return
 	}
 
+	/*
+		함수 종료 시 conn을 종료한다.
+	*/
 	defer conn.Close()
 
-	for {
-		msgType, p, err := conn.ReadMessage()
-		fmt.Println(string(p))
+	Clients[conn] = true
 
+	for {
+		/*
+			클라이언트 수신 메시지를 읽고 출력한다.
+		*/
+		msgType, p, err := conn.ReadMessage()
 		if err != nil {
 			log.Printf("conn.ReadMessage : %v\n", err)
-			return
 		}
 
-		if err := conn.WriteMessage(msgType, p); err != nil {
-			log.Printf("conn.WriteMessage : %v\n", err)
-			return
+		data := ClientMsgInfo{}
+		json.Unmarshal([]byte(p), &data)
+		fmt.Println(string(p))
+
+		Msg := Message{
+			MsgType: msgType,
+		}
+
+		if data.MsgType == "LOGIN" {
+			Msg.MsgInfo.Msg = p
+			Broadcast <- Msg
+		} else {
+			Msg.MsgInfo.Msg = p
+			Broadcast <- Msg
 		}
 	}
 }
 
 func main() {
 
-	http.Handle("/", http.FileServer(http.Dir("page")))
+	/*
+		설정한 url 에 접근했을 때 설정한 파일 서버를 동작시킨다.
+	*/
+	http.Handle("/", http.FileServer(http.Dir("login")))
+	/*
+		ws는 websocket의 약자
+	*/
 	http.HandleFunc("/ws", socketHandler)
 
+	/*
+		broadcast 고루틴 실행
+	*/
+	go BroadcastHandler()
+
+	/*
+		설정 포트로 웹 서버를 실행한다.
+	*/
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe : ", err)
